12b1842031e6ba56b1e532530226181b5d0ac5c8
devuslee <115479127+devuslee@users.noreply.github.com>
2024-04-02 21:22:01 +0800

    View password
    Halfway done, missing when click need change icon function

--- app/src/main/java/com/example/foodcatering/SignUpActivity.java
+++ app/src/main/java/com/example/foodcatering/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.foodcatering;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,8 +13,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -52,13 +57,7 @@ public class SignUpActivity extends AppCompatActivity {
                     if (email.contains("@")) {
                         if (password.length() >= 6) {
                             if (password.equals(confirmPassword)) {
-                                signupconfirmPassword.setError("Password does not match");
-                                HelperClass helperClass = new HelperClass(name, email, password);
-                                reference.child(email).setValue(helperClass);
-
-                                Toast.makeText(SignUpActivity.this, "User Registered Successfully", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                                startActivity(intent);
+                                checkUser();
                             } else {
                                 signupconfirmPassword.setError("Password does not match");
                             }
@@ -83,6 +82,36 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+    }
+
+    public void checkUser() {
+        String name = signupUsername.getText().toString();
+        String email = signupEmail.getText().toString().replace(".", ",");
+        String password = signupPassword.getText().toString();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkEmailDatabase = reference.orderByChild("email").equalTo(email);
+        checkEmailDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    signupEmail.setError("Such user already exist");
+                } else {
+                    HelperClass helperClass = new HelperClass(name, email, password);
+                    reference.child(email).setValue(helperClass);
+
+                    Toast.makeText(SignUpActivity.this, "User Registered Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
